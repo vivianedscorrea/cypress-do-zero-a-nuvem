5f8b0a55b38a3ec2cc274883ddadce4d3e144491
vivianedscorrea <vivianecorreasv@gmail.com>
2025-12-03 17:16:05 -0300

    Lidando com links que abrem em outra aba

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -183,7 +183,7 @@ describe('Aplicação Local Central de Atendimento ao Cliente TAT', () => {
       })
     })
 
-    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
       cy.fixture('example.json').as('sampleFile')
       cy.get('#file-upload')
       .selectFile('@sampleFile')
@@ -191,5 +191,17 @@ describe('Aplicação Local Central de Atendimento ao Cliente TAT', () => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
     })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+      cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+    })
     
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+      cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+      cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+      })  
   }) 
